f0406399bb5a3a27f3b89d48d04078a08e396679
xusQ520 <xusQ520@163.com>
2018-11-05 10:09:45 +0800

    提交

---
+++ vuenames/src/pages/xiaoxhktx.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        我是消息还款提醒
+    </div>
+</template>
+
+<script>
+export default {
+    name:'xiaoxhktx'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vuenames/src/pages/xiaoxtzbz.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        消息通知标注
+    </div>
+</template>
+
+<script>
+export default {
+        name:'xiaoxtzbz'
+}
+</script>
+
+<style scoped>
+
+</style>
