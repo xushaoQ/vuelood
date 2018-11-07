e8381dc78509db9073bdc5706c074f7b057d6a06
apengyou <16619911479@163.com>
2018-11-07 10:44:46 +0800

    提交

---
+++ vuenames/src/pages/chanpin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <header class="header">
+
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'box'
+}
+</script>
+
+<style>
+
+</style>
